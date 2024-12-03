f4936fab0aadbad0bce0be3df59a8ebf723202c7
spoyodevelop <kjy9505@gmail.com>
2024-12-03 14:47:24 +0900

    feat: 코치 validation 추가

---
+++ src/Validator/coachNameValidation.js
@@ -0,0 +1,21 @@
+class coachNameValidation {
+  constructor(coachNames) {
+    this.#validate(coachNames);
+  }
+
+  #validate(coachNames) {
+    if (coachNames === '' || !coachNames.includes(',')) {
+      throw new Error('[ERROR]: 코치 이름은 빌수 없습니다.');
+    }
+    const parsedCoach = coachNames.trim().split(',');
+    if (parsedCoach.some((coach) => coach.length > 4 || coach.length < 2)) {
+      throw new Error('[ERROR]: 코치의 이름은 최소 2글자, 최대 4글자.');
+    }
+    if (parsedCoach.length < 2 || parsedCoach.length > 5) {
+      throw new Error(
+        '[ERROR]: 코치는 최소 2명, 최대 5명까지 식사를 함께 한다.',
+      );
+    }
+    return parsedCoach;
+  }
+}
